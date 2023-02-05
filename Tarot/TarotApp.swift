1376540cd4b680f7240e6b3975e2a81f16cebdd8
Amanda Pinsker <apinsker4@gmail.com>
2023-02-05 16:59:54 -0500

    Initial Commit

---
+++ Tarot/TarotApp.swift
@@ -0,0 +1,17 @@
+//
+//  TarotApp.swift
+//  Tarot
+//
+//  Created by Amanda Pinsker on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TarotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
