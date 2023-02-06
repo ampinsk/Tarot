7a931a9eb007806d29a6fabe7311e1b05d34bce5
Amanda Pinsker <apinsker4@gmail.com>
2023-02-05 20:04:55 -0500

    Initial card layout

--- Tarot/ContentView.swift
+++ Tarot/ContentView.swift
@@ -9,18 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Spacer()
+            Text("The")
+                .font(.custom("NewSpiritTRIAL-Regular", size: 16))
+            Text("Fool")
+                .font(.custom("NewSpiritTRIAL-SemiBold", size: 36))
+            Spacer()
+            HStack() {
+                Text("1")
+                    .font(.custom("NewSpiritTRIAL-Regular", size: 86))
+                Spacer()
+                VStack(alignment: .leading, spacing: 12.0) {
+                    Text("New beginnings, innocence, adventure")
+                        .font(.custom("NewSpiritTRIAL-Regular", size: 12))
+                    Text("Recklessness, fearlessness, risk")
+                        .font(.custom("NewSpiritTRIAL-Regular", size: 12))
+                }
+                .frame(width: 150.0)
+            }
         }
-        .padding()
+        .foregroundColor(.white)
+        .frame(width: 240.0, height: 400.0)
+        .padding(50)
+        .background(Color(.red))
+        .clipShape(RoundedRectangle(cornerRadius: 40, style: .continuous))
+        .overlay(
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+            .stroke(.white)
+            .padding()
+        )
+        .shadow(radius: 10)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 14")
     }
 }
