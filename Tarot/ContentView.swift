1fa21166a3c92551f13a3a5f84c33db476c9bc6f
Amanda Pinsker <amandapinsker@retool.com>
2024-01-30 20:44:22 -0500

    the rest of navigation view

--- Tarot/ContentView.swift
+++ Tarot/ContentView.swift
@@ -6,33 +6,69 @@
 //
 
 import SwiftUI
+import Swift
 
 struct ContentView: View {
     let tarot = TarotInfo()
     
     var body: some View {
-        NavigationView {
-            List() {
-                NavigationLink(destination: MajorArcana() ) {
-                    Text("Major Arcana")
-                }
-                NavigationLink(destination: Cups() ) {
-                    Text("Cups")
-                }
-                NavigationLink(destination: MajorArcana() ) {
-                    Text("Wands")
-                }
-                NavigationLink(destination: MajorArcana() ) {
-                    Text("Swords")
-                }
-                NavigationLink(destination: MajorArcana() ) {
-                    Text("Pentacles")
+        NavigationStack {
+            ZStack {
+                Color.black
+                    .ignoresSafeArea()
+                ScrollView {
+                    Text("Look up a Tarot meaning")
+                        .foregroundColor(.white)
+                        .font(.custom("NewSpiritTRIAL-SemiBold", size: 36))
+                    NavigationLink(destination: MajorArcana() ) {
+                        NavItem(
+                            buttonText: "Major Arcana",
+                            Major: true,
+                            NavColor: Color.gray
+                        )
+                    }
+                    LazyVGrid(columns: [
+                        GridItem(.flexible(), spacing: -70),
+                        GridItem()
+                    ], spacing: 10) {
+                        
+                        NavigationLink(destination: Cups() ) {
+                            NavItem(
+                                buttonText: "Cups",
+                                Major: false,
+                                NavColor: Color.blue
+                            )
+                        }
+                        NavigationLink(destination: MajorArcana() ) {
+                            NavItem(
+                                buttonText: "Pentacles",
+                                Major: false,
+                                NavColor: Color.yellow
+                            )
+                        }
+                        NavigationLink(destination: MajorArcana() ) {
+                            NavItem(
+                                buttonText: "Wands",
+                                Major: false,
+                                NavColor: Color.green
+                            )
+                        }
+                        NavigationLink(destination: MajorArcana() ) {
+                            NavItem(
+                                buttonText: "Swords",
+                                Major: false,
+                                NavColor: Color.red
+                            )
+                        }
+                        
+                    }
                 }
             }
-            .navigationTitle("Tarot")
         }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- Tarot/NavItem.swift
+++ Tarot/NavItem.swift
@@ -9,12 +9,26 @@ import SwiftUI
 
 struct NavItem: View {
     let buttonText: String
+    let Major: Bool
+    let NavColor: Color
     
     var body: some View {
-        Text(buttonText)
+        VStack {
+            Text(buttonText)
+                .frame(width: Major ? 300 : 150, height: Major ? 180 : 200)
+            .foregroundColor(.black)
+            .font(.custom("NewSpiritTRIAL-SemiBold", size: 24))
+        }
+        .background(NavColor)
+        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    NavItem(buttonText: "Hello")
+    NavItem(
+        buttonText: "Hello",
+        Major: true,
+        NavColor: Color.blue
+    )
 }
